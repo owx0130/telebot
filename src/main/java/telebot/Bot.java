1bfc025573155a510cb695a3ee10cc390d8e6e7e
owx0130 <weixiang0130@gmail.com>
2026-02-06 12:49:01 +0800

    init

--- src/main/java/telebot/Bot.java
+++ src/main/java/telebot/Bot.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Bot implements LongPollingSingleThreadUpdateConsumer {
     private final TelegramClient telegramClient;
