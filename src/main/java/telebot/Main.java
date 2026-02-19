bb0164b5e7c9429f87353bb7ad6d5ba47dc4618f
owx0130 <weixiang0130@gmail.com>
2026-02-19 13:57:59 +0800

    fix env variables

--- src/main/java/telebot/Main.java
+++ src/main/java/telebot/Main.java
@@ -1,6 +1,5 @@
 package telebot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
@@ -9,9 +8,8 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     static void main() {
-        Dotenv dotenv = Dotenv.load();
-        String botToken = dotenv.get("BOT_TOKEN");
-        String redisUrl = dotenv.get("REDIS_URL");
+        String botToken = System.getenv("BOT_TOKEN");
+        String redisUrl = System.getenv("REDIS_URL");
 
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(botToken, new Bot(botToken, redisUrl));
