f28e7cf5e56eab8f9857005b283fd62ddbc5528e
owx0130 <weixiang0130@gmail.com>
2026-02-19 15:33:47 +0800

    create dummy server

--- src/main/java/telebot/Main.java
+++ src/main/java/telebot/Main.java
@@ -1,8 +1,11 @@
 package telebot;
 
+import com.sun.net.httpserver.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+import java.net.InetSocketAddress;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -10,6 +13,16 @@ public class Main {
     static void main() {
         String botToken = System.getenv("BOT_TOKEN");
         String redisUrl = System.getenv("REDIS_URL");
+        int port = Integer.parseInt(System.getenv("PORT"));
+
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.start();
+
+            System.out.println("Dummy server running on port " + port);
+        } catch (Exception e) {
+            logger.error("Dummy server failed to start!");
+        }
 
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(botToken, new Bot(botToken, redisUrl));
